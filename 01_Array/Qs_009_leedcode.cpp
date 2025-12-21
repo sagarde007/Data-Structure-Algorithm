d14fe72174fa5615b0e19f44273583ac181704a3
sagarde007 <sagarde2004@gmail.com>
2025-12-21 14:01:45 +0530

    ..

---
+++ 01_Array/Qs_009_leedcode.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0 ;
+        int mid =0;
+        int high = nums.size()-1;
+
+        while(mid <=high){
+
+            if(nums[mid] == 0){
+                swap(nums[low] , nums[mid]);
+                low++;
+                mid++;
+
+            }
+            else if(nums[mid] == 1 ){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+
+        }
+    }
+};
+    
+int main(){
+    // leedcode problem ---> 75
+    // https://leetcode.com/problems/sort-colors/description/
+    return 0;
+}
