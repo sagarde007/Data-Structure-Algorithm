d122c2126f5f5310b0439a5f2d940c26459f97e0
sagar De <sagarde2004@gmail.com>
2026-04-21 09:51:12 +0530

    ..

--- 10_Graph/Qs_016_gfg.cpp
+++ 10_Graph/Qs_016_gfg.cpp
@@ -3,41 +3,59 @@
 using namespace std;
 
 
-bool valid(int i , int j , int n , int m){
-    return (i>=0 && j>=0 && i<n && j<m);
-}
-
-{
-    if(!A[0][0])
-    return -1;
-
-    if(x==0 && y==0)
-    return 0;
-    queue<pair<int , pair<int , int>>>q;
-    q.push({0 , {0,0}});
-    vector<vector<bool>>visited(n,vector<bool>(M,0));
-    visited[0][0] = 1;
-    while(!q.empty()){
-        int i = q.front().first;
-        int j = q.front().second.first;
-        int step = q.front().second.second;
-        q.pop();
-
-        for(int k=0 ; k<4;k++){
-            int new_i = i+row[k];
-            int new_j = J + col[k];
-
-            if(valid(new_i , new_j , N,M) && A[new_i][new_j] && visited[new_i][new_j]){
-                if(new_i == x && new_j == y)
-                return step + 1;
-
-                visited[new_i][new_j] = 1;
-                q.push({new_i , {new_j , step+1}});
-            }
-        }
+class Solution {
+  public:
+    bool valid(int i , int j , int n , int m){
+        return (i>=0 && j>=0 && i<n && j<m);
     }
-    return -1;
-}
+
+    int row[4] = {1 , -1 , 0 , 0};
+    int col[4] = {0 , 0 , 1 , -1};
+
+    int shortestDistance(int N, int M, vector<vector<int>> A, int X, int Y) {
+
+        if(!A[0][0]) return -1;
+        if(X == 0 && Y == 0) return 0;
+
+        // store: {i, j, steps}
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        
+        int step =0;
+        
+        A[0][0] =0;
+
+        while(!q.empty()){
+            int count = q.size();
+            
+            while(count--){
+
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+
+                for(int k = 0; k < 4; k++){
+                    int ni = i + row[k];
+                    int nj = j + col[k];
+
+                    if(valid(ni, nj, N, M) && A[ni][nj]){
+
+                        if(ni == X && nj == Y)
+                            return step + 1;
+                            
+                        A[ni][nj] = 0;
+                        q.push({ni ,nj});
+                        
+                    }
+                }
+            
+            }
+            step++;
+        }
+
+        return -1;
+    }
+};
     
 int main(){
     gfg problem ---> shortest source to destination path
