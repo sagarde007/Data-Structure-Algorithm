9bd5d52c427fc2a24c141d30103bf7db375ba704
sagar De <sagarde2004@gmail.com>
2026-06-08 23:53:15 +0530

    LinkedList

---
+++ 05_Linked List/Qs_023_leedcode.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+    
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+        return head;
+
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *evenHead = even;
+        while(even != NULL && even->next!=NULL){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = even->next->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
+int main(){
+    // leedcode problem -->328
+    // https://leetcode.com/problems/odd-even-linked-list/description/?envType=problem-list-v2&envId=linked-list
+    return 0;
+}
